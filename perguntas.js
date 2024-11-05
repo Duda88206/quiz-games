1578d2b0693013b41c2bf1837db23eb2b89c6e29
Maria Eduarda <00001085307323sp@al.educacao.sp.gov.br>
2024-11-05 08:01:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'genshin impact',
+    'Quantos personagens jogaveis tem em genshin impact?',
+    '91'
+)
+
+criaCartao(
+    'fnaf',
+    'Quantos jogos tem a franquia de FNAF?',
+    'nove jogos'
+)
+
+criaCartao(
+    'fnaf',
+    'Qual alma esta no golden freddy?',
+    'Cassidy'
+)
+
+criaCartao(
+    'genshin impact',
+    'Quais os 3 melhores DPS do jogo?',
+    'Neuvilette,Arleccino e Alhaitan'
+)
